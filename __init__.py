0b4f44508001a39b62330c2fd6bc0c3d7ccca1cc
sanlian114514 <147971457+sanlian114514@users.noreply.github.com>
2024-10-10 22:27:55 +0800

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,34 @@
+import requests,hashlib
+from collections import OrderedDict
+
+def uploadFile(file:bytes,token,type=""):
+    """上传文件函数
+
+    Args:
+        file (bytes):byte类型的文件
+        type (str):文件后缀
+
+    Returns:
+        str:返回信息
+    """
+    utoken=requests.get("https://chat-go.jwzhd.com/v1/misc/qiniu-token2",headers={"token":token}).json()["data"]["token"]
+    uhost=requests.get(f"https://api.qiniu.com/v4/query?ak={utoken.split(':')[0]}&bucket=chat68-file").json()["hosts"][0]["up"]["domains"][0]
+    md5=hashlib.md5(file)
+    key=md5.hexdigest()
+    params = OrderedDict([("token", (None, utoken)),
+                  ("key", (None, key+type)),
+                  ("file", (None, file))])
+    ret=requests.post("https://"+uhost,files=params).json()
+    return ret
+
+def downloadFile(key):
+    """下载文件函数
+
+    Args:
+        key (str):上传时返回的key
+
+    Returns:
+        bytes:文件内容
+    """
+    ret=requests.get("https://chat-file.jwznb.com/"+key,headers={"referer":"http://myapp.jwznb.com"}).content
+    return ret
